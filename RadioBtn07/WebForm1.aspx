5bf0dacb63e82404c022a0b1800f24188b38189c
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2020-09-28 16:44:58 -0600

    Ultimos cambios realizados 27 set 2020

---
+++ RadioBtn07/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RadioBtn07.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:RadioButton ID="rbtnPizza" runat="server" GroupName="alimentos" Text="Pizza" />
+            <asp:RadioButton ID="rbtnFrutas" runat="server" GroupName="alimentos" Text="Frutas" />
+            <asp:RadioButton ID="rbtnVerduras" runat="server" GroupName="alimentos" Text="Verduras" />
+            <br />
+            <asp:Button ID="btnProcesa" runat="server" OnClick="btnProcesa_Click" Text="Procesa" />
+            <br />
+            <asp:RadioButton ID="rbtnGasolina" runat="server" GroupName="combustible" OnCheckedChanged="rbtnGasolina_CheckedChanged" Text="Gasolina" />
+            <asp:RadioButton ID="rbtnDiesel" runat="server" AutoPostBack="True" GroupName="combustible" OnCheckedChanged="rbtnDiesel_CheckedChanged" Text="Diesel" />
+            <asp:RadioButton ID="rbtnEthanol" runat="server" GroupName="combustible" Text="Ethanol" />
+            <br />
+            <asp:Button ID="btnCombustible" runat="server" OnClick="btnCombustible_Click" Text="Combustible" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ RadioBtn07/WebForm1.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RadioBtn07
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnProcesa_Click(object sender, EventArgs e)
+        {
+            if (rbtnPizza.Checked)
+                Response.Write("Te recomiendo hawainana <br>");
+
+            if (rbtnFrutas.Checked)
+                Response.Write("Te recomiendo la manzana <br>");
+
+            if (rbtnVerduras.Checked)
+                Response.Write("Te recomiendo espinacas <br>");
+        }
+
+        protected void btnCombustible_Click(object sender, EventArgs e)
+        {
+            if (rbtnGasolina.Checked)
+                Response.Write("Tu auto usa gasolina <br>");
+            if (rbtnDiesel.Checked)
+                Response.Write("Tu auto usa diesel <br>");
+            if (rbtnEthanol.Checked)
+                Response.Write("Tu auto usa gasolina <br>");
+        }
+
+        protected void rbtnGasolina_CheckedChanged(object sender, EventArgs e)
+        {
+            Response.Write("La gasolina es contamiante <br>");
+        }
+
+        protected void rbtnDiesel_CheckedChanged(object sender, EventArgs e)
+        {
+            Response.Write("El diesel es contamiante <br>");
+        }
+    }
+}
